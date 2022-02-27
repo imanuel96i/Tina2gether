1c434ac5b92c5926cc0b8dad9b57fa4e19bfae1e
Manuel Vidal <imanuel96i@gmail.com>
2022-02-27 00:20:28 -0300

    Deploy

---
+++ src/bot.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const Discord = require('discord.js');
+const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] });
+const { DiscordTogether } = require('discord-together');
+
+client.discordTogether = new DiscordTogether(client);
+
+client.on('messageCreate', async message => {
+    if (message.content === '!2gether') {
+        if(message.member.voice.channel) {
+            client.discordTogether.createTogetherCode(message.member.voice.channel.id, 'youtube').then(async invite => {
+                return message.channel.send(`${invite.code}`);
+            });
+        };
+    };
+});
+
+client.login(process.env.TOKEN);
